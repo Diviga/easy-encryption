0c3743ea0b7358d1997711060eb3569c0e531d97
Diviga <48351099+Diviga@users.noreply.github.com>
2019-03-08 11:26:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,20 @@
-#include <stdio.h>
-#include <string.h>
-#include <string>
-#include <iostream>
-#include <stdio.h>
-#include <ctype.h>
-#include "encrypt.h"
+#include "b64.h"
 
-using namespace std;
+using namespace easy_encryption::vigenere::crypt_operations;
 
-int main() {
- 	// std::string msg = "HELLO WORLD";
- 	std::string msg = "{\"id\":1,\"method\":\"service.subscribe\",\"params\":[\"myapp/0.1c\", null,\"0.0.0.0\",\"80\"]}";
- 	std::string key = "THISISMYKEY";
- 	std::cout << "  message to send: " << msg << std::endl;
- 	std::string encrypted_msg = encrypt(msg, key);
- 	std::cout << "encrypted message: " << encrypted_msg << std::endl;
- 	std::string decrypted_msg = decrypt(encrypted_msg, key);
- 	std::cout << "decrypted message: " << decrypted_msg << std::endl;
-    return 0;
+int main()
+{
+	crypt c;
+ 
+	std::string msg = "{\"id\":1,\"method\":\"service.subscribe\",\"params\":[\"myapp/0.1c\", null,\"0.0.0.0\",\"80\"]}";
+	std::string key = "THISISMYKEY";
+	std::cout << "  message to send: " << msg << std::endl;
+	std::string encrypted_msg = c.encrypt(msg, key);
+	std::cout << "encrypted message: " << encrypted_msg << std::endl;
+	std::string decrypted_msg = c.decrypt(encrypted_msg, key);
+	std::cout << "decrypted message: " << decrypted_msg << std::endl;
+
+	std::cin.get();
+	return 0;
+
 }
